bdb6b1e536b053424135c56796cd9fed4fcc58d2
vince.zipparro <vince.zipparro@GXPC024380>
2018-02-20 10:29:47 -0600

    Add project files.

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        //constructor 
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        //adds grade to list
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+
+    }
+}
